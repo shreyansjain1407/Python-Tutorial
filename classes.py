11f3b996f65dd5175ae37ca7c9c3daddf5b510d7
Shreyans Jain <42056701+shreyansjain1407@users.noreply.github.com>
2022-01-01 16:41:11 -0500

    Modules and Classes

--- classes.py
+++ classes.py
@@ -1,2 +1,29 @@
 # A class is like a blueprint for creating objects. An object has properties and methods(functions) associated with it. Almost everything in Python is an object
 
+#Creation
+class User:
+    #Constructor
+    def __init__(self, name, email, age):
+        self.name = name
+        self.email = email
+        self.age = age
+    
+    def greeting(self):
+        return f'My name is {self.name} and I am {self.age} years ald'
+
+#Initialize user object
+shreyans = User("Shreyans", "test@test.com", 23)
+print(shreyans.name) #Accessing properties
+print(shreyans.greeting())
+
+
+#Extending a class
+class Customer(User):
+    def __init__(self, name, email, age):
+        self.name = name
+        self.email = email
+        self.age = age
+        self.balance = 0
+
+    def set_balance(self, balance):
+        self.balance = balance
--- modules.py
+++ modules.py
@@ -1,1 +1,25 @@
 # A module is basically a file containing a set of functions to include in your application. There are core python modules, modules you can install using the pip package manager (including Django) as well as custom modules
+
+## Core Python modules
+import datetime #*
+from datetime import date #**
+import time
+#Once a module has been installed using pip, you can import it
+from camelcase import CamelCase
+
+#Import custom modules
+import validator
+
+today = datetime.date.today() #*
+today = date.today() #**
+print(today)
+
+timestamp = time.time()
+print(timestamp)
+
+c = CamelCase()
+print(c.hump("hello world everything is capitalized"))
+
+email = "test@test.com"
+if validator.validate_email(email):
+    print("Email is valid")
