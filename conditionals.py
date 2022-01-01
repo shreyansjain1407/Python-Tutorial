752e5d04d19e98a8393228151e6848fbd8b5357d
Shreyans Jain <42056701+shreyansjain1407@users.noreply.github.com>
2022-01-01 15:59:28 -0500

    Fxns, Conditionals, Loops

--- conditionals.py
+++ conditionals.py
@@ -1,19 +1,44 @@
 # If/ Else conditions are used to decide to do something based on something being true or false
 
-
+x = 15
+y = 10
 
 # Comparison Operators (==, !=, >, <, >=, <=) - Used to compare values
 
+if x > y:
+    print(f"{x} is greater than {y}")
 
-
+#If Else
+if x > y:
+    print(f"{x} is greater than {y}")
+else:
+    print(f"{y} is greater than {x}")
 # Logical operators (and, or, not) - Used to combine conditional statements
+if x > y:
+    print(f"{x} is greater than {y}")
+elif x < y:
+    print(f"{y} is greater than {x}")
+else:
+    print(f"{x} is equal to {y}")
 
+#Nested Statements
+if x > y:
+    if x > 5:
+        print(f"{x} is greater than {y} and 5")
 
+if x > y and x > 5: #or 
+    print(f"{x} is greater than {y} and 5")
 
+if not (x == y):
+    print(f"{x} is not equal {y}")
 
 # Membership Operators (in, not in) - Membership operators are used to test if a sequence is presented in an object
 
-
-
+numbers = [1,2,3,4,5,6,7,8,9]
+if 3 in numbers: #not in
+    print("3 is present in the list numbers")
 
 # Identity Operators (is, is not) - Compare the objects, not if they are equal, but if they are actually the same object, with the same memory location:
+
+if 15 is 15: #is not
+    print("Something")
--- loops.py
+++ loops.py
@@ -1,5 +1,27 @@
 # A for loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set, or a string).
+people = ['SJ', 'KB', 'RG', 'GV', 'RN']
+
+#Simple for loop
+for person in people:
+    print(f"Current person: {person}")
+
+#break and continue
+for person in people:
+    if person is 'RN':
+        break
+    if person is 'GV':
+        continue
+    print(f"Current person: {person}")
 
 
+# for i in range(len(people)):
+#     print(people[i])
 
+# for i in range(0, 11):
+#     print(i)
 # While loops execute a set of statements as long as a condition is true.
+
+count = 0
+while(count < 10):
+    print(f"Value of count is: {count}")
+    count += 1
